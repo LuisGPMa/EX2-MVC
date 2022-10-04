ffdde23f73a14a942a53e046264007c0a40de196
Eduardo Spode Cardoso <Eduardo.Spode01@edu.pucrs.br>
2022-10-04 00:42:43 -0300

    Foi

--- Persistencia/Entidades/Department.cs
+++ Persistencia/Entidades/Department.cs
@@ -23,6 +23,8 @@ namespace Persistencia.Entidades
 
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
